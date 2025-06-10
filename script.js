7f707387c88552e227cdf7086ead5f7e63d68a05
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-06-10 20:21:53 +0500

    Added input parsing and result display

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function Calculator() {
         result = 0;
     };
     this.operate = function (a, b, op) {
-        operand1 = a;
-        operand2 = b;
+        operand1 = Number(a);
+        operand2 = Number(b);
         switch (op) {
             case '+':
                 operation = op;
@@ -77,29 +77,29 @@ const display = document.querySelector('.display-area');
 const buttonParent = document.querySelector('.button-area');
 
 const numericButtons = {
-    num0Btn: 0,
-    num1Btn: 1,
-    num2Btn: 2,
-    num3Btn: 3,
-    num4Btn: 4,
-    num5Btn: 5,
-    num6Btn: 6,
-    num7Btn: 7,
-    num8Btn: 8,
-    num9Btn: 9,
+    num0Btn: '0',
+    num1Btn: '1',
+    num2Btn: '2',
+    num3Btn: '3',
+    num4Btn: '4',
+    num5Btn: '5',
+    num6Btn: '6',
+    num7Btn: '7',
+    num8Btn: '8',
+    num9Btn: '9',
 }
 
 const operatorButtons = {
-    addBtn: "+",
-    subtractBtn: "-",
-    multiplyBtn: "*",
-    divideBtn: "/",
-    resultBtn: "=",
+    addBtn: '+',
+    subtractBtn: '-',
+    multiplyBtn: '*',
+    divideBtn: '/',
 }
 
 const methodButtons = {
     // decimalBtn: ".",
     clearBtn: clearDisplay,
+    resultBtn: computeResult,
     // deleteBtn: "",
     // emptyBtn: "",
 }
@@ -107,8 +107,9 @@ const methodButtons = {
 function setDisplay(str) {
     display.textContent = str;
 }
-function clearDisplay(initial = '') {
-    display.textContent = initial;
+function clearDisplay() {
+    display.textContent = 0;
+    resetCalculation();
 }
 
 setDisplay(0);
@@ -117,10 +118,50 @@ buttonParent.addEventListener('click', (e) => {
     let btnId = e.target.id;
 
     if (btnId in numericButtons) {
-        setDisplay(numericButtons[btnId]);
-    }else if(btnId in operatorButtons) {
-        setDisplay(operatorButtons[btnId]);
-    }else if(btnId in methodButtons) {
-        methodButtons[btnId](0);
+        handleNumeric(numericButtons[btnId]);
+    }
+    else if (btnId in operatorButtons) {
+        handleOperator(operatorButtons[btnId]);
+    }
+    else if (btnId in methodButtons) {
+        methodButtons[btnId]();
     }
 });
+
+let operandOne = '';
+let operandTwo = '';
+let operator = '';
+let hasOperator = false;
+
+function resetCalculation() {
+    myCalculator.reset();
+    operandOne = '';
+    operandTwo = '';
+    operator = '';
+    hasOperator = false;
+}
+
+function handleNumeric(digit) {
+    if (!hasOperator) {
+        operandOne += digit;
+        setDisplay(operandOne);
+    }
+    else {
+        operandTwo += digit;
+        setDisplay(operandTwo);
+    }
+}
+function handleOperator(op) {
+    if (!hasOperator && operandOne !== '') {
+        operator = op;
+        hasOperator = true;
+        setDisplay(operator)
+    }
+}
+function computeResult() {
+    if (operandOne !== '' && hasOperator && operandTwo !== '') {
+        let result = myCalculator.operate(operandOne, operandTwo, operator);
+        setDisplay(result);
+        resetCalculation();
+    }
+}
